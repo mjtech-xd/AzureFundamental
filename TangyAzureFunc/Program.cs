d1cdbb943c201ea4099c334c9d6b1ea32962a991
Manoj Jaiswal <manoj.jaiswal883@gmail.com>
2026-05-08 15:19:04 +0530

    Add initial project structure and basic files for Azure Function application

---
+++ TangyAzureFunc/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TangyAzureFunc.Data;
+
+var builder = FunctionsApplication.CreateBuilder(args);
+
+builder.ConfigureFunctionsWebApplication();
+string? connectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+builder.Services
+    .AddApplicationInsightsTelemetryWorkerService()
+    .ConfigureFunctionsApplicationInsights();
+
+builder.Build().Run();
